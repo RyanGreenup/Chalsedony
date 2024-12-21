b22c9da068039b283e5ed92c2183f5be0a9bd3db
Ryan G <exogenesis@protonmail.com>
2024-12-21 18:57:41 +1100

    feat: Add custom light and dark color palettes for application theme

--- draftsmith/main.py
+++ draftsmith/main.py
@@ -152,55 +152,308 @@ class BaseWindowWithMenus(QMainWindow):
         status_bar.showMessage("Ready")
 
 
+def create_light_palette() -> QPalette:
+    palette = QPalette()
+
+    colors = {
+        "Rosewater": {
+            "Hex": "#f4dbd6",
+        },
+        "Flamingo": {
+            "Hex": "#f0c6c6",
+        },
+        "Pink": {
+            "Hex": "#f5bde6",
+        },
+        "Mauve": {
+            "Hex": "#c6a0f6",
+        },
+        "Red": {
+            "Hex": "#ed8796",
+        },
+        "Maroon": {
+            "Hex": "#ee99a0",
+        },
+        "Peach": {
+            "Hex": "#f5a97f",
+        },
+        "Yellow": {
+            "Hex": "#eed49f",
+        },
+        "Green": {
+            "Hex": "#a6da95",
+        },
+        "Teal": {
+            "Hex": "#8bd5ca",
+        },
+        "Sky": {
+            "Hex": "#91d7e3",
+        },
+        "Sapphire": {
+            "Hex": "#7dc4e4",
+        },
+        "Blue": {
+            "Hex": "#8aadf4",
+        },
+        "Lavender": {
+            "Hex": "#b7bdf8",
+        },
+        "Text": {
+            "Hex": "#cad3f5",
+        },
+        "Subtext 1": {
+            "Hex": "#b8c0e0",
+        },
+        "Subtext 0": {
+            "Hex": "#a5adcb",
+        },
+        "Overlay 2": {
+            "Hex": "#939ab7",
+        },
+        "Overlay 1": {
+            "Hex": "#8087a2",
+        },
+        "Overlay 0": {
+            "Hex": "#6e738d",
+        },
+        "Surface 2": {
+            "Hex": "#5b6078",
+        },
+        "Surface 1": {
+            "Hex": "#494d64",
+        },
+        "Surface 0": {
+            "Hex": "#363a4f",
+        },
+        "Base": {
+            "Hex": "#24273a",
+        },
+        "Mantle": {
+            "Hex": "#1e2030",
+        },
+        "Crust": {
+            "Hex": "#181926",
+        },
+    }
+
+    # Set the palette colors
+    palette.setColor(QPalette.Window, QColor(colors["Base"]["Hex"]))
+    palette.setColor(QPalette.WindowText, QColor(colors["Text"]["Hex"]))
+    palette.setColor(QPalette.Base, QColor(colors["Overlay 0"]["Hex"]))
+    palette.setColor(QPalette.AlternateBase, QColor(colors["Overlay 1"]["Hex"]))
+    palette.setColor(QPalette.ToolTipBase, QColor(colors["Text"]["Hex"]))
+    palette.setColor(QPalette.ToolTipText, QColor(colors["Text"]["Hex"]))
+    palette.setColor(QPalette.Text, QColor(colors["Subtext 0"]["Hex"]))
+    palette.setColor(QPalette.Button, QColor(colors["Surface 1"]["Hex"]))
+    palette.setColor(QPalette.ButtonText, QColor(colors["Text"]["Hex"]))
+    palette.setColor(QPalette.BrightText, QColor(colors["Red"]["Hex"]))
+    palette.setColor(QPalette.Link, QColor(colors["Blue"]["Hex"]))
+    palette.setColor(QPalette.Highlight, QColor(colors["Sapphire"]["Hex"]))
+    palette.setColor(QPalette.HighlightedText, QColor(colors["Text"]["Hex"]))
+
+    return palette
+
+
+def create_other_light_palette() -> QPalette:
+    palette = QPalette()
+
+    colors = {
+        "Rosewater": {
+            "Hex": "#dc8a78",
+        },
+        "Flamingo": {
+            "Hex": "#dd7878",
+        },
+        "Pink": {
+            "Hex": "#ea76cb",
+        },
+        "Mauve": {
+            "Hex": "#8839ef",
+        },
+        "Red": {
+            "Hex": "#d20f39",
+        },
+        "Maroon": {
+            "Hex": "#e64553",
+        },
+        "Peach": {
+            "Hex": "#fe640b",
+        },
+        "Yellow": {
+            "Hex": "#df8e1d",
+        },
+        "Green": {
+            "Hex": "#40a02b",
+        },
+        "Teal": {
+            "Hex": "#179299",
+        },
+        "Sky": {
+            "Hex": "#04a5e5",
+        },
+        "Sapphire": {
+            "Hex": "#209fb5",
+        },
+        "Blue": {
+            "Hex": "#1e66f5",
+        },
+        "Lavender": {
+            "Hex": "#7287fd",
+        },
+        "Text": {
+            "Hex": "#4c4f69",
+        },
+        "Subtext 1": {
+            "Hex": "#5c5f77",
+        },
+        "Subtext 0": {
+            "Hex": "#6c6f85",
+        },
+        "Overlay 2": {
+            "Hex": "#7c7f93",
+        },
+        "Overlay 1": {
+            "Hex": "#8c8fa1",
+        },
+        "Overlay 0": {
+            "Hex": "#9ca0b0",
+        },
+        "Surface 2": {
+            "Hex": "#acb0be",
+        },
+        "Surface 1": {
+            "Hex": "#bcc0cc",
+        },
+        "Surface 0": {
+            "Hex": "#ccd0da",
+        },
+        "Base": {
+            "Hex": "#eff1f5",
+        },
+        "Mantle": {
+            "Hex": "#e6e9ef",
+        },
+        "Crust": {
+            "Hex": "#dce0e8",
+        },
+    }
+
+    # Set the palette colors for dark theme
+    palette.setColor(QPalette.Window, QColor(colors["Crust"]["Hex"]))
+    palette.setColor(QPalette.WindowText, QColor(colors["Text"]["Hex"]))
+    palette.setColor(QPalette.Base, QColor(colors["Surface 0"]["Hex"]))
+    palette.setColor(QPalette.AlternateBase, QColor(colors["Surface 1"]["Hex"]))
+    palette.setColor(QPalette.ToolTipBase, QColor(colors["Surface 2"]["Hex"]))
+    palette.setColor(QPalette.ToolTipText, QColor(colors["Text"]["Hex"]))
+    palette.setColor(QPalette.Text, QColor(colors["Subtext 0"]["Hex"]))
+    palette.setColor(QPalette.Button, QColor(colors["Overlay 1"]["Hex"]))
+    palette.setColor(QPalette.ButtonText, QColor(colors["Text"]["Hex"]))
+    palette.setColor(QPalette.BrightText, QColor(colors["Red"]["Hex"]))
+    palette.setColor(QPalette.Link, QColor(colors["Blue"]["Hex"]))
+    palette.setColor(QPalette.Highlight, QColor(colors["Sapphire"]["Hex"]))
+    palette.setColor(QPalette.HighlightedText, QColor(colors["Surface 0"]["Hex"]))
+
+    return palette
+
+
 def create_dark_palette() -> QPalette:
     palette = QPalette()
 
-    # Base colors
-    base_dark = QColor(35, 35, 35)
-    widget_dark = QColor(53, 53, 53)
-    highlight = QColor(42, 130, 218)
-    disabled_gray = QColor(127, 127, 127)
+    colors = {
+        "Rosewater": {
+            "Hex": "#f5e0dc",
+        },
+        "Flamingo": {
+            "Hex": "#f2cdcd",
+        },
+        "Pink": {
+            "Hex": "#f5c2e7",
+        },
+        "Mauve": {
+            "Hex": "#cba6f7",
+        },
+        "Red": {
+            "Hex": "#f38ba8",
+        },
+        "Maroon": {
+            "Hex": "#eba0ac",
+        },
+        "Peach": {
+            "Hex": "#fab387",
+        },
+        "Yellow": {
+            "Hex": "#f9e2af",
+        },
+        "Green": {
+            "Hex": "#a6e3a1",
+        },
+        "Teal": {
+            "Hex": "#94e2d5",
+        },
+        "Sky": {
+            "Hex": "#89dceb",
+        },
+        "Sapphire": {
+            "Hex": "#74c7ec",
+        },
+        "Blue": {
+            "Hex": "#89b4fa",
+        },
+        "Lavender": {
+            "Hex": "#b4befe",
+        },
+        "Text": {
+            "Hex": "#cdd6f4",
+        },
+        "Subtext 1": {
+            "Hex": "#bac2de",
+        },
+        "Subtext 0": {
+            "Hex": "#a6adc8",
+        },
+        "Overlay 2": {
+            "Hex": "#9399b2",
+        },
+        "Overlay 1": {
+            "Hex": "#7f849c",
+        },
+        "Overlay 0": {
+            "Hex": "#6c7086",
+        },
+        "Surface 2": {
+            "Hex": "#585b70",
+        },
+        "Surface 1": {
+            "Hex": "#45475a",
+        },
+        "Surface 0": {
+            "Hex": "#313244",
+        },
+        "Base": {
+            "Hex": "#1e1e2e",
+        },
+        "Mantle": {
+            "Hex": "#181825",
+        },
+        "Crust": {
+            "Hex": "#11111b",
+        },
+    }
 
-    # Window and base
-    palette.setColor(QPalette.ColorRole.Window, widget_dark)
-    palette.setColor(QPalette.ColorRole.WindowText, Qt.GlobalColor.white)
-    palette.setColor(QPalette.ColorRole.Base, base_dark)
-    palette.setColor(QPalette.ColorRole.AlternateBase, widget_dark)
-
-    # Text and buttons
-    palette.setColor(QPalette.ColorRole.Text, Qt.GlobalColor.white)
-    palette.setColor(QPalette.ColorRole.Button, widget_dark)
-    palette.setColor(QPalette.ColorRole.ButtonText, Qt.GlobalColor.white)
-
-    # Selection and highlights
-    palette.setColor(QPalette.ColorRole.Highlight, highlight)
-    palette.setColor(QPalette.ColorRole.HighlightedText, Qt.GlobalColor.black)
-
-    # Menu specific (replaces QSS)
-    palette.setColor(QPalette.ColorRole.Light, widget_dark)  # For menu borders
-    palette.setColor(QPalette.ColorRole.Mid, widget_dark)  # For menu separators
-    palette.setColor(QPalette.ColorRole.Dark, base_dark)  # For pressed states
-
-    # Tooltips
-    palette.setColor(QPalette.ColorRole.ToolTipBase, highlight)
-    palette.setColor(QPalette.ColorRole.ToolTipText, Qt.GlobalColor.white)
-
-    # Disabled state
-    palette.setColor(
-        QPalette.ColorGroup.Disabled, QPalette.ColorRole.WindowText, disabled_gray
-    )
-    palette.setColor(
-        QPalette.ColorGroup.Disabled, QPalette.ColorRole.Text, disabled_gray
-    )
-    palette.setColor(
-        QPalette.ColorGroup.Disabled, QPalette.ColorRole.ButtonText, disabled_gray
-    )
-    palette.setColor(
-        QPalette.ColorGroup.Disabled, QPalette.ColorRole.Highlight, QColor(80, 80, 80)
-    )
-    palette.setColor(
-        QPalette.ColorGroup.Disabled, QPalette.ColorRole.HighlightedText, disabled_gray
-    )
+    # Set the palette colors for a mocha theme
+    palette.setColor(QPalette.Window, QColor(colors["Base"]["Hex"]))
+    palette.setColor(QPalette.WindowText, QColor(colors["Text"]["Hex"]))
+    palette.setColor(QPalette.Base, QColor(colors["Mantle"]["Hex"]))
+    palette.setColor(QPalette.AlternateBase, QColor(colors["Crust"]["Hex"]))
+    palette.setColor(QPalette.ToolTipBase, QColor(colors["Surface 0"]["Hex"]))
+    palette.setColor(QPalette.ToolTipText, QColor(colors["Text"]["Hex"]))
+    palette.setColor(QPalette.Text, QColor(colors["Subtext 0"]["Hex"]))
+    palette.setColor(QPalette.Button, QColor(colors["Overlay 1"]["Hex"]))
+    palette.setColor(QPalette.ButtonText, QColor(colors["Text"]["Hex"]))
+    palette.setColor(QPalette.BrightText, QColor(colors["Rosewater"]["Hex"]))
+    palette.setColor(QPalette.Link, QColor(colors["Blue"]["Hex"]))
+    palette.setColor(QPalette.Highlight, QColor(colors["Sapphire"]["Hex"]))
+    palette.setColor(QPalette.HighlightedText, QColor(colors["Base"]["Hex"]))
 
     return palette
 
@@ -247,7 +500,7 @@ def main(
     if use_dark:
         app.setPalette(create_dark_palette())
     else:
-        app.setPalette(app.style().standardPalette())
+        app.setPalette(create_light_palette())
 
     window = MainWindow(api_url="http://example.com/api")
     signal.signal(signal.SIGINT, signal.SIG_DFL)
