a038cd24a6855878427bd16114817d3d25104106
Ryan G <exogenesis@protonmail.com>
2024-12-22 14:45:13 +1100

    refactor: Fix typing issues in NoteView setup_ui method

--- draftsmith/note_view.py
+++ draftsmith/note_view.py
@@ -27,22 +27,25 @@ class NoteView(QWidget):
         self.left_sidebar = QFrame()
         self.left_sidebar.setObjectName("leftSidebar")
         self.left_sidebar.setFrameShape(QFrame.Shape.StyledPanel)
-        self.left_sidebar.setLayout(QVBoxLayout())
-        self.left_sidebar.layout().setContentsMargins(0, 0, 0, 0)
+        left_layout = QVBoxLayout()
+        left_layout.setContentsMargins(0, 0, 0, 0)
+        self.left_sidebar.setLayout(left_layout)
 
         # Main content area
         self.content_area = QFrame()
         self.content_area.setObjectName("contentArea")
         self.content_area.setFrameShape(QFrame.Shape.StyledPanel)
-        self.content_area.setLayout(QVBoxLayout())
-        self.content_area.layout().setContentsMargins(0, 0, 0, 0)
+        content_layout = QVBoxLayout()
+        content_layout.setContentsMargins(0, 0, 0, 0)
+        self.content_area.setLayout(content_layout)
 
         # Right sidebar
         self.right_sidebar = QFrame()
         self.right_sidebar.setObjectName("rightSidebar")
         self.right_sidebar.setFrameShape(QFrame.Shape.StyledPanel)
-        self.right_sidebar.setLayout(QVBoxLayout())
-        self.right_sidebar.layout().setContentsMargins(0, 0, 0, 0)
+        right_layout = QVBoxLayout()
+        right_layout.setContentsMargins(0, 0, 0, 0)
+        self.right_sidebar.setLayout(right_layout)
 
         # Add frames to splitter
         self.main_splitter.addWidget(self.left_sidebar)
