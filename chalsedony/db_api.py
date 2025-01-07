e541cf04982bb68f2e9438953f75a2551e4ce13f
Ryan G <exogenesis@protonmail.com>
2025-01-07 13:31:34 +1100

    Create initial Database API

---
+++ chalsedony/db_api.py
@@ -0,0 +1,10 @@
+from typing import Optional, BinaryIO, Literal, List
+from pydantic import BaseModel, Field
+from datetime import datetime
+from pathlib import Path
+from decimal import Decimal
+from enum import Enum
+import requests
+from urllib.parse import quote
+
+
