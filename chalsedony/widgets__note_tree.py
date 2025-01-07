59be4a1b0bae1d0c4afcd765c0da7c5d7585b2e6
Ryan G <exogenesis@protonmail.com>
2025-01-07 13:37:10 +1100

    refactor: Remove unused `_add_folder_to_tree` method from `NoteTree` class

--- chalsedony/widgets__note_tree.py
+++ chalsedony/widgets__note_tree.py
@@ -25,23 +25,6 @@ class NoteTree(QTreeWidget):
         for folder in root_folders:
             self._add_folder_to_tree(folder, self)
 
-    def _add_folder_to_tree(
-        self, folder: TreeTagWithNotes, parent: QTreeWidget | QTreeWidgetItem
-    ) -> None:
-        """Recursively add a folder and its contents to the tree"""
-        folder_item = QTreeWidgetItem(parent)
-        folder_item.setText(0, folder.name)
-        folder_item.setData(0, Qt.ItemDataRole.UserRole, ("folder", folder.id))
-
-        # Add notes in this folder
-        for note in folder.notes:
-            note_item = QTreeWidgetItem(folder_item)
-            note_item.setText(0, note.title)
-            note_item.setData(0, Qt.ItemDataRole.UserRole, ("note", note.id))
-
-        # Recursively add subfolders
-        for subfolder in folder.children:
-            self._add_folder_to_tree(subfolder, folder_item)
 
     def show_context_menu(self, position: QPoint) -> None:
         """Show context menu with create action"""
