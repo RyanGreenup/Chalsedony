36d96c9422229882aa92386e5fbc522dd193e15d
Ryan G <exogenesis@protonmail.com>
2025-01-11 17:36:58 +1100

    refactor: Add docstring to _connect_signals and remove AI comment

--- chalsedony/widgets__search_tab.py
+++ chalsedony/widgets__search_tab.py
@@ -89,8 +89,8 @@ class NoteListWidget(KbdListWidget):
         self.create_keybinings()
         self._connect_signals()
 
-    # Modify this to not emit anything if there is no item selected AI!
     def _connect_signals(self) -> None:
+        """Connect signals and ensure they only emit when an item is selected."""
         def get_item_data(item: QListWidgetItem) -> TreeItemData:
             return TreeItemData(
                 ItemType.NOTE,
